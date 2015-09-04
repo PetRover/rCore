342d4b07490e3f89ce98d45e7702f496ad1405df
Alyssa Bezreh <bezreh.a@husky.neu.edu>
2015-09-04 13:56:22 -0400

    Export PWM functionality into startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+echo cape-bone-iio > /sys/devices/bone_capemgr.8/slots
+
 echo bspm_P8_41_17 > /sys/devices/bone_capemgr.8/slots
-echo 74>/sys/class/gpio/export
+echo 74 > /sys/class/gpio/export
+
+echo 4 > /sys/class/pwm/export
