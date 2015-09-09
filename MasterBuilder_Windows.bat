19da7af9dcab71ec10ecff5a65c6d066bb643ace
Bryce Carter <brycedcarter@gmail.com>
2015-09-09 16:32:33 -0400

    -Bryce is a dumb dumb

--- MasterBuilder_Windows.bat
+++ MasterBuilder_Windows.bat
@@ -2,8 +2,8 @@ mkdir -p bin
 mkdir -p build
 cd build
 
-cmake -DCMAKE_TOOLCHAIN_FILE=BBBToolchain_Mac.cmake ..
-#cmake -DCMAKE_TOOLCHAIN_FILE=BBBToolchain_Mac.cmake -DCMAKE_VERBOSE_MAKEFILE=ON ..
+cmake -DCMAKE_TOOLCHAIN_FILE=BBBToolchain_Windows.cmake ..
+#cmake -DCMAKE_TOOLCHAIN_FILE=BBBToolchain_Windows.cmake -DCMAKE_VERBOSE_MAKEFILE=ON ..
 make
 scp ../bin/rCore root@192.168.7.2:/home/root/rover
 scp ../bin/rTests root@192.168.7.2:/home/root/rover
